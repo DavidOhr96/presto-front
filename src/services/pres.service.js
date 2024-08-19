5716cdef0498cb749f9b1cb236332b84188db826
David Ohr <our.david96@gmail.com>
2024-08-19 14:34:28 +0300

    UPDATE slide service CRUD functions

--- src/services/pres.service.js
+++ src/services/pres.service.js
@@ -1,1 +1,18 @@
 import { httpService } from './http.service.js'
+
+
+const STORAGE_KEY = 'pres'
+
+export const presService = {
+query,
+getByTitle
+}
+window.cs = presService
+
+async function query(){
+    return httpService.get(STORAGE_KEY)
+}
+
+async function getByTitle(presTitle){
+    return httpService.get(`pres/${presTitle}`)
+}
--- src/services/slide.service.js
+++ src/services/slide.service.js
@@ -1,1 +1,35 @@
 import { httpService } from './http.service.js'
+
+
+const STORAGE_KEY = 'slide'
+
+export const slideService = {
+query,
+getById,
+save,
+remove
+}
+window.cs = slideService
+
+async function query(){
+    return httpService.get(STORAGE_KEY)
+}
+
+async function getById(slideId){
+    return httpService.get(`slide/${slideId}`)
+}
+
+async function save(slide){
+    let savedSlide
+    if(slide._id){
+        savedSlide=await httpService.put(`slide/${slide._id}`,slide)
+    }else{
+        savedSlide=await httpService.post('slide'.car)
+    }
+    return savedSlide
+    }
+
+
+    async function remove(slideId){
+        return httpService.delete(`slide/${slideId}`)
+    }
